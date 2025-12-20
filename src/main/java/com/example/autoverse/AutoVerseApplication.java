5215cf999ba871245c0a21263454dca87e2cd1fd
Arif Imeri <arifimeri.11@gmail.com>
2025-12-20 23:24:06 +0100

    first commit

---
+++ src/main/java/com/example/autoverse/AutoVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoVerseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autoverse/AutoVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
